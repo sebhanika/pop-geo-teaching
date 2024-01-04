0ee945f861f1f5b3cfc5831753f5b68776697a3c
sebhanika <hanikasebastian@gmail.com>
2024-01-04 11:39:54 +0100

    update settings

--- scripts/0_settings.r
+++ scripts/0_settings.r
@@ -1,8 +1,22 @@
 # Title: Settings file
-# Date: 2023-09-22
+# Date: 2024-01-04
 # Purpose: This is a settings file that will be tracked by git
+# /* cSpell:disable */
 
-
+library(ggthemes)
 options(scipen = 999)
 
 # GGplot Settings  --------------
+
+basic_theme <- theme_set(theme_base() +
+    theme(
+        axis.line = element_line(colour = "black"),
+        axis.title = element_text(size = 16, color = "grey30"),
+        axis.text = element_text(size = 16, color = "grey30"),
+        legend.text = element_text(size = 18, color = "grey30"),
+        legend.title = element_blank()
+    ))
+
+# other code snippets
+
+`%!in%` <- Negate(`%in%`) # function needed for later
