284ae765b65158c4c0d9b2610c094ee9073eabdd
sebhanika <hanikasebastian@gmail.com>
2024-01-18 15:20:31 +0100

    init commit

---
+++ scripts/le_map_europe.r
@@ -0,0 +1,15 @@
+# Title: Life Expectancy
+# Date: 2024-01-04
+# Purpose: Life expectancy for selected countries
+# /* cSpell:disable */
+
+# Library --------------
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+library(HMDHFDplus)
+library(countrycode)
+library(nationalparkcolors)
+
+source("scripts/0_config.R")
+source("scripts/0_settings.R")
