3df3ae0b7fda7b6b7399949acae8713e2a9f7683
sebhanika <hanikasebastian@gmail.com>
2024-01-22 11:41:43 +0100

    update code

--- scripts/mortality/le_map_world.r
+++ scripts/mortality/le_map_world.r
@@ -3,6 +3,7 @@
 # Purpose: Life expectancy map world
 # /* cSpell:disable */
 
+# Libraries --------------
 library(dplyr)
 library(tidyr)
 library(wbstats)
@@ -14,13 +15,17 @@ library(gganimate)
 library(transformr)
 library(gifski)
 
-
 source("scripts/0_config.R")
 source("scripts/0_settings.R")
 
+# palette for maps
+self_palette <- c("#eff3ff", "#bdd7e7", "#6baed6", "#3182bd", "#08519c")
+
+
+# Load data --------------
+
 le_dat <- wb_data("SP.DYN.LE00.FE.IN", start_date = 1960, end_date = 2021) %>%
     rename(le_female = SP.DYN.LE00.FE.IN)
-
 
 le_world <- cshp(date = as.Date("2019-01-01")) %>%
     mutate(iso3c = countrycode(gwcode,
@@ -28,13 +33,12 @@ le_world <- cshp(date = as.Date("2019-01-01")) %>%
         destination = "iso3c"
     ))
 
+
+# Map 2019 --------------
+
 le_sf2019 <- le_world %>%
     left_join(subset(le_dat, date == "2019"))
 
-# creating custom color palette
-self_palette <- c("#eff3ff", "#bdd7e7", "#6baed6", "#3182bd", "#08519c")
-
-# create bins for chrolopeth map
 data_bins <- BAMMtools::getJenksBreaks(le_sf2019$le_female, k = 6)
 
 data_labs <- paste(
@@ -50,7 +54,6 @@ le_sf2019_2 <- le_sf2019 %>%
         include.lowest = TRUE
     ))
 
-# Plot Map 2019 --------------
 plot_le_sf2 <- le_sf2019_2 %>%
     ggplot() +
     geom_sf(aes(fill = as.factor(val_int)),
@@ -79,21 +82,15 @@ ggsave(
 )
 
 
-
-
-
-
 # Animated map --------------
 
-# This code does not work currently,
-# There is a bug in the gganimate/transformr package
+# This code is currently a work around due to a bug
+# in the gganimate/transformr package
 # however installing previous versions does not work
-# on this laptop. Will investigage further
+# on this laptop. W
 
 
-
-# lets make four maps with facet wrap instead
-
+# making map frames manually and add them with gfiski
 le_dat_gif <- le_world %>%
     left_join(le_dat)
 
@@ -113,10 +110,8 @@ le_dat_gif2 <- le_dat_gif %>%
         include.lowest = TRUE
     ))
 
-
 yrs <- 1960:2021
 map_list <- list()
-
 
 for (i in yrs) {
     map_list[[as.character(i)]] <- le_dat_gif2 %>%
@@ -143,10 +138,6 @@ for (i in yrs) {
 }
 
 
-
-
-
-
 # save all plots in list as pngs
 invisible(
     lapply(
@@ -167,54 +158,8 @@ invisible(
     )
 )
 
-
 png_files <- list.files("viszs/le_map/", pattern = ".*png$", full.names = TRUE)
 gifski(png_files,
     gif_file = "viszs/le_world_map.gif",
     width = 1209, height = 680, delay = 0.25
-)
-
-
-
-
-
-
-
-
-# Old code with bug  --------------
-
-
-# Plot Map GIF
-plot_map_all_years <- le_dat_gif2 %>%
-    ggplot() +
-    geom_sf(aes(fill = as.factor(val_int)),
-        linewidth = 0.1, alpha = 1
-    ) +
-    scale_fill_manual("Female Life Expectancy",
-        values = self_palette,
-        na.value = "#a7a7a7"
-    ) +
-    labs(
-        caption = "Source: World Bank (2023)"
-    ) +
-    theme(
-        axis.text = element_blank(),
-        axis.ticks = element_blank(),
-        legend.position = "bottom"
-    )
-
-
-plot_map_gif <- plot_map_all_years +
-    transition_time(date)
-
-
-
-
-
-
-
-anim_save("viszs/world_female_le.gif", plot_map_gif,
-    fps = 2,
-    renderer = gifski_renderer(loop = TRUE),
-    height = 32, width = 18, units = "cm", res = 150
 )
