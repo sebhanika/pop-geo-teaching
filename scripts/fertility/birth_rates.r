c0ddef2a46c907f35672def517bc470e2c758c56
sebhanika <hanikasebastian@gmail.com>
2024-01-25 11:47:38 +0100

    init commit

---
+++ scripts/fertility/birth_rates.r
@@ -0,0 +1,15 @@
+# Title: birth_rates
+# Date: 2024-01-25
+# Purpose: births in sweden
+# /* cSpell:disable */
+
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+library(HMDHFDplus)
+library(ggthemes)
+library(countrycode)
+library(nationalparkcolors)
+
+source("scripts/0_config.R")
+source("scripts/0_settings.R")
